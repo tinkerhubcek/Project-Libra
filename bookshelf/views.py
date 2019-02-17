995f7a65433b187d8ffee24b0da4933dfc6a96b0
Athul CA <athul8720@gmail.com>
2019-02-17 18:40:58 +0530

    added count of books and barcodes

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book,Log_user,A_Logger,Author,BarCode
 # Create your views here.
 def main_page(request):
-    return render(request, 'bookshelf/index.html', {book:'book'})
+    num_books = Book.objects.all().count()
+    bar_num= BarCode.objects.all().count()
+    context = {
+        'book': num_books,
+        'bar_code':bar_num,
+        #'num_instances': num_instances,
+        #'num_instances_available': num_instances_available,
+        #'num_authors': num_authors,
+    }
+    
+    return render(request, 'bookshelf/index.html', context=context)
