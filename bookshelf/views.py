515977e0972ff44c5f708590528ff8f25acddc83
Athul CA <athul8720@gmail.com>
2019-02-18 09:42:29 +0530

    Added Users and Log_User rendering from DB

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -6,11 +6,14 @@ def main_page(request):
     num_books = Book.objects.all().count()
     bar_num= BarCode.objects.all().count()
     user_num= Log_user.objects.all().count()
+    ln_books= A_Logger.objects.filter(status__exact='On loan').count()
+    avail_books=A_Logger.objects.filter(status__exact='Available').count()
     context = {
         'book': num_books,
         'bar_code':bar_num,
         'num_users': user_num,
-        #'num_instances_available': num_instances_available,
+        'book_onloan': ln_books,
+        'book_av':avail_books,
         #'num_authors': num_authors,
     }
     
