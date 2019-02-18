28d2b0e467b84112f46d6dd2d230a24f01e58672
Athul CA <athul8720@gmail.com>
2019-02-18 11:54:14 +0530

    Updated a typo

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -7,13 +7,14 @@ def main_page(request):
     bar_num= BarCode.objects.all().count()
     user_num= Log_user.objects.all().count()
     ln_books= A_Logger.objects.filter(status__exact='On loan').count()
-    avail_books=A_Logger.objects.filter(status__exact='Available').count()
+    #avail_books=A_Logger.objects.filter(status__exact ='On loan').count()
+    av_book = num_books-ln_books
     context = {
         'book': num_books,
         'bar_code':bar_num,
         'num_users': user_num,
         'book_onloan': ln_books,
-        'book_av':avail_books,
+        'book_av':av_book,
         #'num_authors': num_authors,
     }
     
