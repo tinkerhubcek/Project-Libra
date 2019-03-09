6267a6fffa3ffc884d5ec04a43641507936061a1
Athul CA <athul8720@gmail.com>
2019-03-09 12:54:12 +0530

    updated urls py

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name='index'),
     path('books/', views.Books.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
