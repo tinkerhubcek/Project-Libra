2da75ddd8503e7a9d9d78c0f63e0e4cacdaeb980
athul <athul8720@gmail.com>
2019-08-31 23:33:58 +0530

    Changed API views to a Simple One :sparkles:

--- bookshelf/serializers.py
+++ bookshelf/serializers.py
@@ -1,25 +1,25 @@
 from rest_framework import serializers
 from .models import *
 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model=Book
         fields=('url','Code','title','author','language','Edition')
-class CodeSerializer(serializers.HyperlinkedModelSerializer):
+class CodeSerializer(serializers.ModelSerializer):
     class Meta:
         model=BarCode
         fields=('__all__')
-class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
 
 
     class Meta:
         model=Author
         fields=('__all__')
-class LoggerSerializer(serializers.HyperlinkedModelSerializer):
+class LoggerSerializer(serializers.ModelSerializer):
     class Meta:
         model=A_Logger
         fields=('__all__')
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=Log_user
         fields=('__all__')
